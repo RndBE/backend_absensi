a3c71ef53d39ccc069560b69c23c9ee99d4a0118
RndBE <rnd.software.be@gmail.com>
2026-07-10 13:09:21 +0700

    gateway lama

--- config/services.php
+++ config/services.php
@@ -56,10 +56,8 @@ return [
     ],
 
     'whatsapp' => [
-        'endpoint' => env('WHATSAPP_ENDPOINT', 'https://whatsapp.beacontelemetry.com/tessa-wa/send/message'),
-        'username' => env('WHATSAPP_USERNAME', 'admin'),
-        'password' => env('WHATSAPP_PASSWORD'),
-        'device_id' => env('WHATSAPP_DEVICE_ID', '1a678778-afb5-479b-8894-f7bbc005841d'),
+        'endpoint' => env('WHATSAPP_ENDPOINT', 'http://72.60.78.159:3000/client/sendMessage/beacon'),
+        'api_key' => env('WHATSAPP_API_KEY'),
         'timeout' => env('WHATSAPP_TIMEOUT', 15),
     ],
 
